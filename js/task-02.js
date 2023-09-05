e28b15ec4416d839ebcede58d92f3d1cd0407329
Vladyslav Bozhuk <94122235+escape23@users.noreply.github.com>
2023-09-05 23:39:26 +0200

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients')
+
+const items = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li')
+  itemEl.classList.add('item');
+  itemEl.textContent = ingredient;
+  return itemEl;
+})
+
+listEl.append(...items);
