d8778a076b164b89f48cbefa455e4f730924538d
Vladyslav Bozhuk <94122235+escape23@users.noreply.github.com>
2023-09-08 09:25:28 +0200

    Finally

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Заповніть всі поля");
+  }
+  const message = { email: email.value, Password: password.value };
+
+  console.log(message);
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.body;
+const changeColorBtn = document.querySelector("button.change-color");
+const spanText = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", (event) => {
+  let color = getRandomHexColor();
+  console.log(color);
+  body.style.backgroundColor = color;
+  spanText.textContent = color;
+});
